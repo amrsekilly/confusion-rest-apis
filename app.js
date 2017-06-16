661a1a2e9b1fed16b1a282c65b2b11ea91bbc4d7
Amr Elsekilly <amr.e.dev@gmail.com>
2017-06-16 14:32:37 +0200

    modified the dishes router

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ var leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
 
+// connect to the mongo DB
+var mongoose = require('mongoose');
+
+var url = 'mongodb://localhost:27017/conFusion';
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("Connected correctly to server");
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
